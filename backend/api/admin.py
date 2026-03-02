57f23423c26eb480c7b0fe18464b15d3952832fc
Subesh Yadav <subeshgaming@gmail.com>
2026-03-02 20:21:25 +0545

    Admin Register

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Artist, Album, Track, Playlist
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Track)
+admin.site.register(Playlist)
